e9cdd837296ff769919ae416fe84e6b68649c3a2
ssemenidov <23sergey03@gmail.com>
2020-09-01 20:14:43 +0800

    username + card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Button, TextField } from "@material-ui/core";
+
 import "./App.css";
 import Message from "./components/Message";
 function App() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState(["gfdgf", "gfdgfd"]);
+  const [username, setUsername] = useState("unsigned user");
   const addMessage = (e) => {
     e.preventDefault();
     setMessages([input, ...messages]);
     setInput("");
   };
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
   return (
     <div className="App">
       <h1>Messanger</h1>
@@ -34,7 +39,7 @@ function App() {
 
       <ul>
         {messages.map((message) => (
-          <Message message={message}></Message>
+          <Message username={username} message={message}></Message>
         ))}
       </ul>
     </div>
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
 
 function Message(props) {
   return (
     <div>
-      <h2>{props.message}</h2>
+      <Typography
+        variant="h8"
+        component="h8"
+        color="textSecondary"
+        gutterBottom
+      >
+        {props.username}
+      </Typography>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {props.message}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
